9aa4e9bcffd7dd703c1ab02e260d50080f02508d
Matheus <matheusbritzke@gmail.com>
2017-10-08 19:47:04 -0300

    added operation create car

--- crudSpringBoot/src/main/java/com/github/matheusbritzke/GreetingController.java
+++ crudSpringBoot/src/main/java/com/github/matheusbritzke/GreetingController.java
@@ -1,20 +1,29 @@
 package com.github.matheusbritzke;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private AllCars cars = new AllCars();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{plate}/{powerRating}")
+    @ResponseBody
+    public String newCar(@PathVariable(value = "plate") String plate, @PathVariable(value = "powerRating") double powerRating){
+        Car car = new Car(plate, powerRating);
+        cars.addCar(car);
+        return car.toString();
+    }
+
     
 }
