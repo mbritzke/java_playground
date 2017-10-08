7a71ab256c06c9878634129f4639ad8660d7beea
Matheus <matheusbritzke@gmail.com>
2017-10-08 20:39:34 -0300

    Greeting class removed

--- crudSpringBoot/src/main/java/com/github/matheusbritzke/CarController.java
+++ crudSpringBoot/src/main/java/com/github/matheusbritzke/CarController.java
@@ -1,22 +1,13 @@
 package com.github.matheusbritzke;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CarController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
     private AllCars cars = new AllCars();
-
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
-    }
 
     @RequestMapping(method = RequestMethod.GET, value = "newcar/{plate}/{powerRating}")
     @ResponseBody
