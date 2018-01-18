256b3621ec28e49fba6705872d66b52dc47b3b5a
mbritzke <matheusbritzke@gmail.com>
2018-01-18 16:35:36 -0200

    Simplificação do Controller

--- dbSpringBoot/src/main/java/com/github/matheusbritzke/controller/BookController.java
+++ dbSpringBoot/src/main/java/com/github/matheusbritzke/controller/BookController.java
@@ -1,62 +1,39 @@
 package com.github.matheusbritzke.controller;
 
 import com.github.matheusbritzke.model.Book;
-import com.github.matheusbritzke.repository.BookRepository;
-
+import com.github.matheusbritzke.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
 
     @Autowired
-    BookRepository bookRepository;
+    private BookService bookService;
 
-    @RequestMapping(method=RequestMethod.GET)
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    @RequestMapping(method = RequestMethod.POST)
+    public Book addNewBook(@RequestBody Book newBook) {
+        return bookService.addBook(newBook);
     }
 
-    @RequestMapping(method=RequestMethod.POST)
-    public Book createBook(@Valid @RequestBody Book book) {
-        return bookRepository.save(book);
+    @RequestMapping(method = RequestMethod.GET)
+    public Iterable<Book> getAllBooks() {
+        return bookService.getAllBooks();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Book> getBookById(@PathVariable(value = "id") Long id) {
-        Book foundBook = bookRepository.findOne(id);
-        if(foundBook == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok().body(foundBook);
+    @RequestMapping(value = "/{bookId}", method = RequestMethod.GET)
+    public Book getBookById(@PathVariable(value = "bookId") Long bookId) {
+        return bookService.getBookById(bookId);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Book> updateBook(@PathVariable(value = "id") Long id,
-                                           @Valid @RequestBody Book bookActual) {
-        Book book = bookRepository.findOne(id);
-        if(book == null) {
-            return ResponseEntity.notFound().build();
-        }
-        book.setName(bookActual.getName());
-        book.setGender(bookActual.getGender());
-
-        Book updatedBook = bookRepository.save(book);
-        return ResponseEntity.ok(updatedBook);
+    @RequestMapping(value = "/{bookId}", method = RequestMethod.PUT)
+    public Book updateBook(@PathVariable(value = "bookId") Long bookId, @RequestBody Book book) {
+        return bookService.updateBook(bookId, book);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Book> deleteBook(@PathVariable(value = "id") Long id) {
-        Book book = bookRepository.findOne(id);
-        if(book == null) {
-            return ResponseEntity.notFound().build();
-        }
-        bookRepository.delete(book);
-        return ResponseEntity.ok().build();
+    @RequestMapping(value = "/{bookId}", method = RequestMethod.DELETE)
+    public Book deleteCar(@PathVariable(value = "bookId") Long bookId) {
+        return bookService.deleteBook(bookId);
     }
 }
