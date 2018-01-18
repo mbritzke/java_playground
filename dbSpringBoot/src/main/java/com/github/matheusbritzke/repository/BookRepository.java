583060a9675c16019f1d7d6fa590c541c84fcef9
mbritzke <matheusbritzke@gmail.com>
2018-01-18 16:34:57 -0200

    Atualização dos imports

--- dbSpringBoot/src/main/java/com/github/matheusbritzke/repository/BookRepository.java
+++ dbSpringBoot/src/main/java/com/github/matheusbritzke/repository/BookRepository.java
@@ -1,9 +1,7 @@
 package com.github.matheusbritzke.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import com.github.matheusbritzke.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long>{
+public interface BookRepository extends CrudRepository<Book, Long>{
 }
