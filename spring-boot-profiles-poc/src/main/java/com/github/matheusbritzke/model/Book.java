3ce9f2dfc65ed2bdbb014f737cb47bd8d12efef2
mbritzke <matheusbritzke@gmail.com>
2018-02-28 16:07:47 -0300

    Ajuste no tipo do atributo id

--- spring-boot-profiles-poc/src/main/java/com/github/matheusbritzke/model/Book.java
+++ spring-boot-profiles-poc/src/main/java/com/github/matheusbritzke/model/Book.java
@@ -1,25 +1,16 @@
 package com.github.matheusbritzke.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class Book{
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-
+    private Integer id;
     private String name;
     private String gender;
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
